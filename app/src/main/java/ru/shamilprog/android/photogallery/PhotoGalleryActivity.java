eabb9ae9e0c1b69936450984b68e668ffc914b90
Shamilprog <shamilprog@yandex.ru>
2020-12-17 00:07:51 +0300

    Initial commit

---
+++ app/src/main/java/ru/shamilprog/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,13 @@
+package ru.shamilprog.android.photogallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_photo_gallery);
+    }
+}
