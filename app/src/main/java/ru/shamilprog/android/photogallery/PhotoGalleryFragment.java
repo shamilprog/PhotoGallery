37cd28da46a69a9f80730221c60a34b82d77ce07
Shamilprog <shamilprog@yandex.ru>
2020-12-25 23:00:49 +0300

    Fixing a little bug.

--- app/src/main/java/ru/shamilprog/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/ru/shamilprog/android/photogallery/PhotoGalleryFragment.java
@@ -11,6 +11,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -19,7 +20,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.io.IOException;
