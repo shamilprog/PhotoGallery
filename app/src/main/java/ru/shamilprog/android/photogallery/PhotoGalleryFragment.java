2206c83686c2f998db5e0c7a85c9521d577c7ea2
Shamilprog <shamilprog@yandex.ru>
2020-12-27 19:23:02 +0300

    Created a service for background fetching of data about current query.

--- app/src/main/java/ru/shamilprog/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/ru/shamilprog/android/photogallery/PhotoGalleryFragment.java
@@ -1,5 +1,6 @@
 package ru.shamilprog.android.photogallery;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -44,6 +45,9 @@ public class PhotoGalleryFragment extends Fragment {
         setRetainInstance(true);
         setHasOptionsMenu(true);
         updateItems();
+
+        Intent i = PollService.newIntent(getActivity());
+        getActivity().startService(i);
 
         Handler responseHandler = new Handler();
         mThumbnailDownloader = new ThumbnailDownloader<>(responseHandler);
