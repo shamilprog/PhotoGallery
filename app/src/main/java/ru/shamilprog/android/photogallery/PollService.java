d414c82c4a3ea470a08e81f7cdd69ffcc58baf7f
Shamilprog <shamilprog@yandex.ru>
2020-12-30 00:16:32 +0300

    Showing notifications about new photos.

--- app/src/main/java/ru/shamilprog/android/photogallery/PollService.java
+++ app/src/main/java/ru/shamilprog/android/photogallery/PollService.java
@@ -2,12 +2,16 @@ package ru.shamilprog.android.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -17,7 +21,7 @@ public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
     // 60 seconds
-    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(15);
 
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
@@ -75,6 +79,23 @@ public class PollService extends IntentService {
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
         }
+
+        Resources resources = getResources();
+        Intent i = PhotoGalleryActivity.newIntent(this);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setTicker(resources.getString(R.string.new_pictures_title))
+                .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                .setContentTitle(resources.getString(R.string.new_pictures_title))
+                .setContentText(resources.getString(R.string.new_pictures_text))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
+                .build();
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+        notificationManager.notify(0, notification);
 
         QueryPreferences.setLastResultId(this, resultId);
     }
